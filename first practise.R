e5eff02ce41027a1947be25a917be0a9c6745d35
gourav1710 <59788961+gourav1710@users.noreply.github.com>
2020-01-12 13:30:30 +0530

    Add files via upload

---
+++ first practise.R
@@ -0,0 +1,47 @@
+3*2
+2+3*4
+3^2
+exp(1)
+sqrt(10)
+pi
+2*pi*6378
+x<-1
+y<-3
+Z<-5
+
+my_vect<-c(1,2,3,5)
+my_vect[4]
+y<-c(2,10,9,5)
+y<9
+my_vect<-c(2,0,0,4)
+my_vect[1]
+my_v<-c(2,0,0,4)
+my_v[-1]
+my_v[1]<-3;my_v
+my_v[-1]<-5;my_v
+my_vect<-c(1,3,5,7,9)
+my_vect[my_vect<5]<-2
+
+
+# Data Frames construction
+
+df<-data.frame(x=1:3,y=c('a','b','c'))
+df
+df1<-data.frame(x=c(1,4,9),y=c('a','b','c'))
+df1
+df[1,2]
+df[1,c(1,2)]
+df[c(1,2),2]
+df[c(3,1),1]
+df[c(1,2),2]
+df[c(3,1),1]
+df[2,c(1,2)]
+df[3,2]
+df[2,c(1,2)]
+df[3,c(1,2)]
+df[ ,2]
+df[1, ]
+
+
+
+# 
